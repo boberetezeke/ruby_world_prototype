16ebd74cfd1edfc1bb72d8e20af756a1e92a3dc9
Steve Tuckner <stevetuckner@gmail.com>
2023-08-13 15:58:52 -0600

    separate out fantasy baseball files

--- app/commands.rb
+++ app/commands.rb
@@ -2,7 +2,7 @@ path = File.dirname(__FILE__)
 
 load "#{path}/commands/save.rb"
 load "#{path}/commands/contacts.rb"
-load "#{path}/commands/players.rb"
+# load "#{path}/commands/players.rb"
 load "#{path}/commands/f.rb"
 load "#{path}/commands/bt.rb"
 load "#{path}/commands/sql.rb"
